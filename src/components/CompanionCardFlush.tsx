2f577ae5afbff94937db2d0b1392cede59d13130
Ryan Le <31494954+lryanle@users.noreply.github.com>
2024-04-21 02:15:05 -0500

    chore(cards): tweaked card stylings

--- src/components/CompanionCardFlush.tsx
+++ src/components/CompanionCardFlush.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import { AnimatePresence, motion } from "framer-motion";
 import { CanvasRevealEffect } from "@/components/ui/canvas-reveal-effect";
 import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
 
 type Props = {};
 
@@ -21,10 +22,10 @@ export default function CompanionCardFlush({}: Props) {
 						possible.
 					</h2>
 					<div className="py-4 flex flex-col lg:flex-row items-center justify-center w-full gap-4 mx-auto px-8">
-						<Card title="Hybrid Mode" description={"Looking to chat and speak to a Poké-companion? Hybrid mode serves best of both worlds and allows for interchangable conversations between voice and text."} cta={"Converse"} >
+						<Card title="Hybrid Mode" description={"Looking to chat and speak to a Poké-companion? Hybrid mode serves best of both worlds and allows for interchangable conversations between voice and text."} cta={"Converse"} className="hover:drop-shadow-[0_15px_15px_rgba(74,4,78,0.25)] hover:-translate-y-1 transition-all duration-300 ease-in-out" >
 							<CanvasRevealEffect
 								animationSpeed={3}
-								containerClassName="bg-black"
+								containerClassName="bg-fuchsia-950"
 								colors={[
 									[236, 72, 153],
 									[232, 121, 249],
@@ -33,14 +34,14 @@ export default function CompanionCardFlush({}: Props) {
 							/>
 							<div className="absolute inset-0 [mask-image:radial-gradient(400px_at_center,white,transparent)] bg-black/20" />
 						</Card>
-						<Card title="Chat Mode" description={"Want to chat with a Poké-companion? Chat mode is the easiest way to type questions and get answers back quickly with minimal delay."} cta={"Message"} >
+						<Card title="Chat Mode" description={"Want to chat with a Poké-companion? Chat mode is the easiest way to type questions and get answers back quickly with minimal delay."} cta={"Message"} className="hover:drop-shadow-[0_15px_15px_rgba(5,150,105,0.25)] hover:-translate-y-1 transition-all duration-300 ease-in-out" >
 							<CanvasRevealEffect
 								animationSpeed={5.1}
-								containerClassName="bg-emerald-900"
+								containerClassName="bg-emerald-600"
 							/>
               <div className="absolute inset-0 [mask-image:radial-gradient(400px_at_center,white,transparent)] bg-black/20" />
 						</Card>
-						<Card title="Voice Mode" description={"Prefer to speak with a Poké-companion? Voice mode allows you to ask questions and get answers back in real-time with the power of your voice."} cta={"Speak"} >
+						<Card title="Voice Mode" description={"Prefer to speak with a Poké-companion? Voice mode allows you to ask questions and get answers back in real-time with the power of your voice."} cta={"Speak"} className="hover:drop-shadow-[0_15px_15px_rgba(2,132,199,0.25)] hover:-translate-y-1 transition-all duration-300 ease-in-out" >
 							<CanvasRevealEffect
 								animationSpeed={3}
 								containerClassName="bg-sky-600"
@@ -60,15 +61,17 @@ const Card = ({
 	children,
   description,
   cta,
+  className,
 }: {
 	title: string;
   description: string;
   cta: string;
 	children?: React.ReactNode;
+  className?: string;
 }) => {
 	return (
 		<div
-			className="rounded-xl overflow-clip border border-black/[0.2] group/canvas-card flex items-center justify-center max-w-[34%] w-full mx-auto p-4 relative max-h-[34%]"
+			className={cn("rounded-xl overflow-clip border border-black/[0.2] group/canvas-card flex items-center justify-center max-w-[34%] w-full mx-auto px-4 py-2 relative max-h-[34%]", className)}
 		>
 
 			<AnimatePresence>
@@ -81,11 +84,11 @@ const Card = ({
 					</motion.div>
 			</AnimatePresence>
 
-			<div className="relative z-20 p-5 drop-shadow-xl">
-				<h1 className="text-2xl relative z-10 font-bold text-white transition duration-200 =drop-shadow-2xl">
+			<div className="relative z-20 px-5 py-4 drop-shadow-xl">
+				<h1 className="text-2xl relative z-10 font-bold text-white transition duration-200 drop-shadow-2xl leading-none">
 					{title}
 				</h1>
-        <h2 className="text-sm text-white drop-shadow-2xl mt-1">{description}</h2>
+        <h2 className="text-sm text-white drop-shadow-2xl mt-2">{description}</h2>
 				<Button variant="secondary" className="mt-3 drop-shadow-2xl">
 					{`${cta} -->`}
 				</Button>
--- src/components/DexCard.tsx
+++ src/components/DexCard.tsx
@@ -11,7 +11,7 @@ type Props = {
 
 export default function DexCard({ title, description, cta, className }: Props) {
 	return (
-		<div className={cn(`rounded-lg bg-gradient-to-b drop-shadow-xl hover:-translate-y-2 hover:drop-shadow-2xl transition-all ease-in-out`, className)}>
+		<div className={cn(`rounded-lg bg-gradient-to-b drop-shadow-xl hover:-translate-y-2 hover:drop-shadow-2xl duration-300 transition-all ease-in-out`, className)}>
 			<div className="bg-crosshatch w-full h-full px-8 py-4 max-w-96">
 				<h1 className="text-2xl font-bold text-foreground">{title}</h1>
 				<h2 className="text-foreground">{description}</h2>
